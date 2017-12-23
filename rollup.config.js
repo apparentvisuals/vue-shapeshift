e14d848f840194f94a7e53a21801e4df353ad27c
apparentvisuals <3787826+apparentvisuals@users.noreply.github.com>
2017-12-23 15:28:35 -0500

    Add .travis.yml

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import strip from 'rollup-plugin-strip';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
